da6a44552dcc8232f6e51268c9bceb493439b323
jaredhall18 <jaredhall18@gmail.com>
2022-01-26 16:54:23 -0700

    Components exported properly, ready for design.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 
 export default function Header() {
   return (
-  <div>
-      <a>Home</a>
-      <a>About</a>
-      <a>Projects</a>
-      <a>Contact</a>
-
-  </div>)
+    <div>
+        <a>Home</a>
+        <a>About</a>
+        <a>Projects</a>
+        <a>Contact</a>
+    </div>
+  )
 
 }
